3a68ef218d0733371a57d4c39c1dc55cdf53ebd3
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-26 02:37:55 +0530

    Add handle deleteMethod and deleted a coffee

--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -1,30 +1,69 @@
+import Swal from "sweetalert2";
 
-const CoffeeCard = ({coffee}) => {
-    const{name,quantity,supplier,taste,category,details,photo}= coffee
-    return (
-        <div className="card card-side bg-base-100 shadow-xl">
-  <figure>
-    <img
-      src={photo}
-      alt="Movie" />
-  </figure>
-  <div className="flex justify-between w-full items-center ">
-  <div>
-  <h2 className="card-title">{name}</h2>
-    <p>Quantity:{quantity}</p>
-    <p>Supplier:{supplier}</p>
-    <p>Taste:{taste}</p>
-  </div>
-    <div className="justify-end ">
-    <div className="join join-vertical space-y-4 p-5">
-  <button className="btn ">View</button>
-  <button className="btn ">Update</button>
-  <button className="btn ">X</button>
-</div>
+const CoffeeCard = ({ coffee }) => {
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`,{
+            method:'DELETE',
+
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+
+        console.log("Delete");
+      }
+    });
+  };
+
+  return (
+    <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src={photo} alt="Movie" />
+      </figure>
+      <div className="flex justify-between w-full items-center ">
+        <div>
+          <h2 className="card-title">{name}</h2>
+          <p>Quantity:{quantity}</p>
+          <p>Supplier:{supplier}</p>
+          <p>Taste:{taste}</p>
+        </div>
+        <div className="justify-end ">
+          <div className="join join-vertical space-y-4 p-5">
+            <button className="btn ">View</button>
+            <button className="btn ">Update</button>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn bg-red-500"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default CoffeeCard;

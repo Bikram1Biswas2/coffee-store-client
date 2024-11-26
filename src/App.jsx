3d82ccbcb2962df2cb9a665935bb6705b810f30f
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-26 13:02:04 +0530

    Deleted from ui implement successfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
 import CoffeeCard from './Components/CoffeeCard'
+import { useState } from 'react'
 
 
 
 function App() {
-  const coffees = useLoaderData()
+  const loadedCoffees = useLoaderData()
+  const[coffees,setCoffees] = useState(loadedCoffees)
 
 
   return (
@@ -14,7 +16,11 @@ function App() {
       <h1 className='text-5xl text-purple-500 text-center'>Hot Hot Cold Cold Coffee: {coffees.length}</h1>
       <div className='grid md:grid-cols-2 gap-5'>
         {
-          coffees.map(coffee=> <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+          coffees.map(coffee=> <CoffeeCard key={coffee._id}
+             coffee={coffee}
+             coffees={coffees}
+             setCoffees={setCoffees}
+             ></CoffeeCard>)
         }
       </div>
     </>

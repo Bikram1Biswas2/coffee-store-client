cfaf9a8d08f877e434f1922f768ba2593f372879
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-11-30 00:17:58 +0530

    Successfully deleted from ui

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,11 +1,43 @@
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const Users = () => {
 
     const loadedUser = useLoaderData()
     const [users,setUsers]= useState(loadedUser)
+
+    const handleDelete = _id =>{
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+           
+            fetch(`http://localhost:5000/users/${_id}`,{
+                method:"DELETE"
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                   Swal.fire({
+                title: "Deleted!",
+                text: "User has been deleted.",
+                icon: "success"
+              });
+              const remaining = users.filter(u => u._id !== _id)
+              setUsers(remaining)
+            })
+            }
+          });
+        }
 
     return (
         <div>
@@ -29,6 +61,10 @@ const Users = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>{user.createdAt}</td>
+                <td>
+                    <button className="btn">Edit</button>
+                    <button onClick={()=>handleDelete(user._id)} className="btn">Delete</button>
+                </td>
               </tr>)
           }
     </tbody>
